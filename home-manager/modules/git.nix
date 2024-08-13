d60941926f41dc8c403b4e9b0dd18843e6d294bd
Dixxe <dixxe.mail@proton.me>
2024-08-13 18:13:47 +0300

    Fixes

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,7 +1,13 @@
+{ pkgs, ... }:
+
 {
     programs.git = {
         enable = true;
         userEmail = "dixxe.mail@proton.me"; # Hello there :D.
         userName = "Dixxe";
     };
+
+    home.packages = with pkgs; [ # Pretty wonky, but idk
+        git-credential-oauth     # how to make it better
+    ];
 }
